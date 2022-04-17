3174ea2af307b67058c8c8e20e8dd5da7a855aad
Suraj <suraj.kedari@cctech.co.in>
2022-04-17 13:16:10 +0530

    New program for true and false

---
+++ True.c
@@ -0,0 +1,8 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int main(void){
+    assert(true==true);
+    return EXIT_SUCCESS;
+}
---
+++ false.c
@@ -0,0 +1,8 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int main(void){
+    assert(false==true);
+    return EXIT_FAILURE;
+}
